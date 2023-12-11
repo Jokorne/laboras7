afd98625a49b02f606ee7a7fb39c2a8ffb8beeb3
Jokorne <150124608+Jokorne@users.noreply.github.com>
2023-12-11 22:54:25 +0200

    Update duomenys.js

--- duomenys.js
+++ duomenys.js
@@ -12,8 +12,23 @@ function submitForm() {
     const feature4 = parseFloat(document.getElementById('feature4').value);
     const feature5 = parseFloat(document.getElementById('feature5').value);
 
-    contactData.firstname = firstName;
-    contactData.lastname = lastName;
+    if (!validateEmail(email)) {
+        alert('Netinkamas el. pašto adresas');
+        return;
+    }
+
+    if (!validatePhone(phone)) {
+        alert('Netinkamas telefono numeris');
+        return;
+    }
+
+    if (!validateAddress(address)) {
+        alert('Adresas yra privalomas');
+        return;
+    }
+
+    contactData.firstName = firstName;
+    contactData.lastName = lastName;
     contactData.email = email;
     contactData.phone = phone;
     contactData.address = address;
@@ -30,8 +45,8 @@ function submitForm() {
     displayDataOnPage('Amžius:', feature4);
     displayDataOnPage('Vaikų skaičius:', feature5);
 
-    displayDataInConsole();
-    calculateAndDisplayAverage(contactData);
+    const average = calculateAverage(contactData.features);
+    displayDataOnPageWithColor(`${firstName} ${lastName} (${email}): ${average.toFixed(2)}`, average);
 }
 
 function displayDataOnPage(label, value) {
@@ -42,22 +57,43 @@ function displayDataOnPage(label, value) {
     contactInfoDiv.appendChild(dataLine);
 }
 
-function displayDataInConsole(contactData) {
-    console.log('Duomenys:', contactData);
+function displayDataOnPageWithColor(value, average) {
+    const contactInfoDiv = document.getElementById('contactInfo');
+
+    const dataLine = document.createElement('p');
+    dataLine.textContent = value;
+    
+    if (average < 10) {
+        dataLine.style.color = 'red';
+    } else if (average >= 10 && average <= 20) {
+        dataLine.style.color = 'orange';
+    } else {
+        dataLine.style.color = 'green';
+    }
+
+    contactInfoDiv.appendChild(dataLine);
 }
 
-function calculateAndDisplayAverage(contactData) {
-    const features = contactData.features;
-    let sum = 0;
+function validateAddress(address) {
+    return address.trim() !== '';
+}
 
+function validateEmail(email) {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+    const phoneRegex = /^\d{9}$/;
+    return phoneRegex.test(phone);
+}
+
+function calculateAverage(features) {
+    let sum = 0;
     for (const feature of features) {
         sum += feature;
     }
-
-    const average = sum / features.length;
-    const vidurkis = document.getElementById('vidurkis');
-
-    vidurkis.textContent = `${contactData.firstname} ${contactData.lastname} (${contactData.email}): ${average.toFixed(2)}`;
+    return sum / features.length;
 }
 
 document.getElementById('submitBtn').addEventListener('click', submitForm);
